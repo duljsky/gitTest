ea837680534311659f6d8fdf140693a440bb1bda
duljsky <991998969@qq.com>
2021-06-12 11:07:26 +0800

    创建Hello.java文件
    使用GitHub创建新文件，体验一下效果

---
+++ Hello.java
@@ -0,0 +1,20 @@
+public class Hello {
+    public static void main(String[] args) {
+        //输出字符串
+        System.out.println("com.fitsco.base.Hello, World!");
+        int a = 10;
+        byte b = 10;
+        short c = 11;
+        long d = 12;
+        float e = 12.2F;
+        double f = 34.23;
+        char g = 's';
+        String h = "sdfjsfdj";
+        boolean bl = true;
+        System.out.println(bl);
+        int money = 10_0000_0000;
+        int year = 20;
+        long total = (long)money*year;
+        System.out.println(total);
+    }
+}
